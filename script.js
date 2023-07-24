a9aab5d65525bcdf097b055b7912a1d90ed2ec83
gopichand909 <100563483+gopichand909@users.noreply.github.com>
2023-07-24 21:31:03 +0530

    Update script.js

--- script.js
+++ script.js
@@ -214,6 +214,13 @@ function fillOval() {
 function fillCenter() {
   // Corner cells: [0, 2, 6, 8]
   const nonCenterCells = [4, 0,2, 6,8, 1, 3 , 5, 7];
+  if (cellElements[1].classList.contains(X_CLASS) && (cellElements[7].classList.contains(X_CLASS) ) && !cellElements[6].classList.contains(X_CLASS) && !cellElements[6].classList.contains(CIRCLE_CLASS) ) {
+    return 6;
+  }
+  
+  if (cellElements[2].classList.contains(X_CLASS) && cellElements[7].classList.contains(X_CLASS) && !cellElements[8].classList.contains(X_CLASS) && !cellElements[8].classList.contains(CIRCLE_CLASS) ) {
+    return 8;
+  }
   if (cellElements[5].classList.contains(X_CLASS) && (cellElements[7].classList.contains(X_CLASS) || cellElements[6].classList.contains(X_CLASS)) && !cellElements[8].classList.contains(X_CLASS) && !cellElements[8].classList.contains(CIRCLE_CLASS) ) {
     return 8;
   }
